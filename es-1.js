748c3953b6df65e8d5f619f3c20414aa05df9bbc
GiuliaCarangelo <tnhkagura@gmail.com>
2024-01-01 19:13:15 +0100

    fix let finale (print assigned word)

---
+++ es-1.js
@@ -0,0 +1,10 @@
+/***
+ * Completare il codice JavaScript successivo in modo che console.log() stampi in console
+ * la stringa "Geronimooo!"
+ */
+
+let finale = '';
+for (let i = 0; i < 3; i++) {
+    finale += 'o';
+}
+console.log('Esercizio 1:', 'Geronim' + finale);
